4c8bb2dd61c3222a20f6af0a7571824c7ee711dc
Loren Gordon <lorengordon@users.noreply.github.com>
2023-09-07 13:01:24 -0700

    Updates method to start moto server, also ensuring script exits on failure

--- tests/terraform_pytest/ready.d/start.sh
+++ tests/terraform_pytest/ready.d/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-nohup "$MAVEN_CONFIG"/.venv/bin/moto_server --host 0.0.0.0 --port 4615 &
+set -eu -o pipefail
+nohup "$PWD"/.venv/bin/moto_server --host 0.0.0.0 --port 4615 &
 echo "Pausing for moto to warm up ..." >&2
 sleep 2
